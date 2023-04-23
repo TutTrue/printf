850641472fd332c1d31b1234092d0be702ab296d
TutTrue <hamdy.xd55@gmail.com>
2023-04-23 14:34:01 -0700

    _printf.c handle va_arg

--- _printf.c
+++ _printf.c
@@ -33,7 +33,15 @@ int _printf(const char *format, ...)
 				if (format[i + 1] == fun[j].ch)
 				{
 					len += fun[j].f(ap);
-					va_arg(ap, int);
+					if (format[i + 1] == 's')
+						va_arg(ap, char *);
+					else if (format[i + 1] == '%')
+					{
+						i += 2;
+						break;
+					}
+					else
+						va_arg(ap, int);
 					i += 2;
 					break;
 				}
