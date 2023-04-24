a24e024909ed5a6c6c8a4e1b4f10a32e60551a70
Elshafae <Elshfae@example.com>
2023-04-24 10:59:44 -0700

    betty,_printf

--- _printf.c
+++ _printf.c
@@ -8,13 +8,12 @@
 int _printf(const char *format, ...)
 {
 	int j, i = 0, len = 0;
+	va_list ap;
 
 	format_t fun[] = {{'c', print_char}, {'s', print_string},
 		{'%', print_mod}, {'i', print_int},
 		{'d', print_int}
 	};
-
-	va_list ap;
 
 	va_start(ap, format);
 
@@ -25,23 +24,14 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			/*if (format [i + 1] == '\0')
-				return (-1);*/
+			if (format[i + 1] == '\0')
+				return (-1);
 			j = 0;
 			while (j < 5)
 			{
 				if (format[i + 1] == fun[j].ch)
 				{
 					len += fun[j].f(ap);
-					/*if (format[i + 1] == 's')
-						va_arg(ap, char *);
-					else if (format[i + 1] == '%')
-					{
-						i += 2;
-						break;
-					}
-					else
-						va_arg(ap, int);*/
 					i += 2;
 					break;
 				}
