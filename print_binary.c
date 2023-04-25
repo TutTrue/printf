6641334fb863047f6c363a6ca016ddbc9d6f10b1
Elshafae <Elshfae@example.com>
2023-04-25 01:42:53 -0700

    print_binary3

--- print_binary.c
+++ print_binary.c
@@ -11,11 +11,7 @@ void print_binary_help(int num, int *len_ptr)
 	unsigned int rem;
 
 	if (num <= 0)
-	{
-		_putchar('0');
-		(void)(*len_ptr)++;
 		return;
-	}
 	rem = num % 2;
 	print_binary_help(num / 2, len_ptr);
 	_putchar(rem + '0');
